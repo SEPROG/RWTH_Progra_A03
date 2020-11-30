f90c223fec97539466fdd6732ad45934273dba2e
seprog <sebastianpoppenborg@gmail.com>
2020-11-30 12:19:29 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Programmierung Aufgabenblatt 03
+ * Aufgabe 7
+ *
+ * @author 422412, Sebastian Poppenborg
+ * @author xxxxxx, Finja     Rang
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

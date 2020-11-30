4cf16d44a55d092640b1ed13bc3618bdd77059e7
seprog <sebastianpoppenborg@gmail.com>
2020-11-30 14:35:41 +0100

    Teilaufgabe 7.c)

--- src/Karte.java
+++ src/Karte.java
@@ -1,10 +1,35 @@
 public class Karte {
 
-    private Farbe farbe;
-    private Wert wert;
+    private final Farbe FARBE;
+    private final Wert WERT;
+
+    private Karte(Farbe farbe, Wert wert) {
+        this.FARBE = farbe;
+        this.WERT = wert;
+    }
 
     @Override
     public String toString() {
-        return (farbe.toString() + wert.toString());
+        return ( this.FARBE.toString() + this.WERT.toString() );
+    }
+
+    /** Gibt eine neue Karten Instanz zurueck.
+     *
+     * @param farbe, die Farbe der neuen Karte.
+     * @param wert, der Wert der neuen Karte.
+     * @return eine neue Instanz vom Typ Karte.
+     */
+    public static Karte neueKarte(Farbe farbe, Wert wert) {
+        return new Karte(farbe, wert);
+    }
+
+    /** Gibt eine neue Karten Instanz zurueck. Die Paramater dürfern als valide Strings angegeben werden.
+     *
+     * @param farbe, die Farbe der neuen Karte. String Repräsentant des Enums Farbe.
+     * @param wert, der Wert der neuen Karte. String Repräsentant des Enums Wert.
+     * @return eine neue Instanz vom Typ Karte.
+     */
+    public static Karte neueKarte(String farbe, String wert) {
+        return neueKarte(Farbe.valueOf(farbe), Wert.valueOf(wert));
     }
 }

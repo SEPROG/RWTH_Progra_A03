26112c73721d2114606b1cbbe73b346911bd4832
seprog <sebastianpoppenborg@gmail.com>
2020-11-30 17:37:13 +0100

    Teilaufgabe 7.g)

---
+++ src/Spieler.java
@@ -0,0 +1,11 @@
+public class Spieler {
+
+    private Karte[] kartenhand;
+    private String name;
+    private double siegesquote;
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}

3e6c4f94f7fc16b37f2dc82a04ae8494240acbd2
seprog <sebastianpoppenborg@gmail.com>
2020-11-30 17:43:37 +0100

    Teilaufgabe 7.h)

--- src/Spieler.java
+++ src/Spieler.java
@@ -8,4 +8,19 @@ public class Spieler {
     public String toString() {
         return this.name;
     }
+
+    /** Gibt den besten Spieler für einen gegebenen Club (Spieler-Array) zurück. Vergleich anhand von siegesquote.
+     *
+     * @param club, das Spieler-Array in dem der beste Spieler festgestellt werden soll.
+     * @return den besten Spieler in club.
+     */
+    public static Spieler besterSpieler(Spieler... club) {
+        Spieler besterSpieler = club[0];
+
+        for (Spieler s : club) {
+            besterSpieler = s.siegesquote > besterSpieler.siegesquote ? s : besterSpieler;
+        }
+
+        return besterSpieler;
+    }
 }

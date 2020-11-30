0ba79823b60a6480eec1ece3569f05468ec0f6c4
seprog <sebastianpoppenborg@gmail.com>
2020-11-30 18:08:13 +0100

    Teilaufgabe 7.j)

--- src/Spieler.java
+++ src/Spieler.java
@@ -4,12 +4,26 @@ public class Spieler {
     private String name;
     private double siegesquote;
 
+    public Spieler(String name) {
+        this.name = name;
+    }
+
+    public Spieler(String name, double siegesquote) {
+        this(name);
+        this.siegesquote = siegesquote;
+    }
+
+    public Spieler(String name, double siegesquote, Karte[] kartenhand) {
+        this(name, siegesquote);
+        this.kartenhand = kartenhand;
+    }
+
     /** Gibt in stdout aus ob dieser Spieler eine bestimmte Karte bedienen kann oder nicht.
      *
      * @param k, die karte die bedient werden soll.
      */
     public void kannBedienen(Karte k) {
-        System.out.println(name + (k.bedienbar(kartenhand) ? "kann bedienen!" : "kann nicht bedienen!"));
+        System.out.println(this.name + (k.bedienbar(this.kartenhand) ? "kann bedienen!" : "kann nicht bedienen!"));
     }
 
     @Override
@@ -30,5 +44,28 @@ public class Spieler {
         }
 
         return besterSpieler;
+    }
+
+    /** Startet das Prgramm. Führt einige Testfunktionen aus.
+     *
+     * @param args, werden nicht beachtet.
+     */
+    public static void main(String[] args) {
+        // Initialize example players
+        Spieler s0 = new Spieler("Elisabeth", 0.375);
+        Spieler s1 = new Spieler("Klaus", 0.125);
+        Spieler s2 = new Spieler("Helmut", 0.3875);
+        Spieler s3 = new Spieler("Erwin", 0.1125);
+        // Set best player
+        Spieler bs = Spieler.besterSpieler(s0, s1, s2, s3);
+
+        // Initialize sample cards
+        Karte[] karten = {Karte.neueKarte(Farbe.HERZ, Wert.SIEBEN), Karte.neueKarte(Farbe.HERZ, Wert.NEUN), Karte.neueKarte(Farbe.KARO, Wert.KOENIG)};
+
+        // Give sample cards to best player
+        bs.kartenhand = karten;
+
+        // Check whether the best player can serve the jack of diamonds
+        bs.kannBedienen(Karte.neueKarte(Farbe.KARO, Wert.BUBE));
     }
 }
